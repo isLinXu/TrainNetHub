5cdd2dd6be2938895c6c7274dc9eb47ff0dffd5d
杂杳 <2267379130@qq.com>
2021-09-14 10:16:59 +0800

    1.yolov5 plots.py 新增注释

--- YOLO/yolov5_master/utils/plots.py
+++ YOLO/yolov5_master/utils/plots.py
@@ -14,8 +14,8 @@ import torch
 import yaml
 from PIL import Image, ImageDraw, ImageFont
 
-from utils.general import xywh2xyxy, xyxy2xywh
-from utils.metrics import fitness
+from YOLO.yolov5_master.utils.general import xywh2xyxy, xyxy2xywh
+from YOLO.yolov5_master.utils.metrics import fitness
 
 # Settings
 matplotlib.rc('font', **{'size': 11})
@@ -26,17 +26,21 @@ class Colors:
     # Ultralytics color palette https://ultralytics.com/
     def __init__(self):
         # hex = matplotlib.colors.TABLEAU_COLORS.values()
+        # 十六进制格式颜色
         hex = ('FF3838', 'FF9D97', 'FF701F', 'FFB21D', 'CFD231', '48F90A', '92CC17', '3DDB86', '1A9334', '00D4BB',
                '2C99A8', '00C2FF', '344593', '6473FF', '0018EC', '8438FF', '520085', 'CB38FF', 'FF95C8', 'FF37C7')
+        # RGB格式颜色
         self.palette = [self.hex2rgb('#' + c) for c in hex]
         self.n = len(self.palette)
 
     def __call__(self, i, bgr=False):
+        # 返回对应颜色
         c = self.palette[int(i) % self.n]
         return (c[2], c[1], c[0]) if bgr else c
 
     @staticmethod
     def hex2rgb(h):  # rgb order (PIL)
+        # 16进制的颜色格式转为RGB格式
         return tuple(int(h[1 + i:1 + i + 2], 16) for i in (0, 2, 4))
 
 
@@ -44,9 +48,17 @@ colors = Colors()  # create instance for 'from utils.plots import colors'
 
 
 def hist2d(x, y, n=100):
+    """
+    labels.png和evolve.png中使用的二维直方图
+    根据x，y的直方图分布，来返回绘制颜色，区间数量多的颜色更亮，反之更暗
+    :param x：ndarray
+    :param y：ndarray
+    """
     # 2d histogram used in labels.png and evolve.png
     xedges, yedges = np.linspace(x.min(), x.max(), n), np.linspace(y.min(), y.max(), n)
+    # 获取直方图，和xy轴范围
     hist, xedges, yedges = np.histogram2d(x, y, (xedges, yedges))
+    # 取得每个x, y在hist上的索引, 区间数量更多的返回的hist就更大，颜色就越亮
     xidx = np.clip(np.digitize(x, xedges) - 1, 0, hist.shape[0] - 1)
     yidx = np.clip(np.digitize(y, yedges) - 1, 0, hist.shape[1] - 1)
     return np.log(hist[xidx, yidx])
@@ -66,6 +78,10 @@ def butter_lowpass_filtfilt(data, cutoff=1500, fs=50000, order=5):
 
 
 def plot_one_box(x, im, color=(128, 128, 128), label=None, line_thickness=3):
+    """使用opencv画单个框
+    :param x: 坐标，xyxy格式左上角右下角
+    :param im:原图
+    """
     # Plots one bounding box on image 'im' using OpenCV
     assert im.data.contiguous, 'Image not contiguous. Apply np.ascontiguousarray(im) to plot_on_box() input image.'
     tl = line_thickness or round(0.002 * (im.shape[0] + im.shape[1]) / 2) + 1  # line/font thickness
@@ -80,7 +96,12 @@ def plot_one_box(x, im, color=(128, 128, 128), label=None, line_thickness=3):
 
 
 def plot_one_box_PIL(box, im, color=(128, 128, 128), label=None, line_thickness=None):
+    """使用Pillow画单个框
+    :param x: 坐标，xyxy格式左上角右下角
+    :param im:原图
+    """
     # Plots one bounding box on image 'im' using PIL
+    # numpy/cv2转化为Pillow格式
     im = Image.fromarray(im)
     draw = ImageDraw.Draw(im)
     line_thickness = line_thickness or max(int(min(im.size) / 200), 2)
@@ -94,6 +115,7 @@ def plot_one_box_PIL(box, im, color=(128, 128, 128), label=None, line_thickness=
 
 
 def plot_wh_methods():  # from utils.plots import *; plot_wh_methods()
+    """画yolov3/yolov5坐标回归的范围"""
     # Compares the two methods for width-height anchor multiplication
     # https://github.com/ultralytics/yolov3/issues/168
     x = np.arange(-4.0, 4.0, .1)
@@ -114,6 +136,10 @@ def plot_wh_methods():  # from utils.plots import *; plot_wh_methods()
 
 
 def output_to_target(output):
+    """将预测输出格式 转化为 标签格式，test.py中可视化预测时使用
+    网络输出格式为:[(N, 6), ] x batch, xyxy, conf, cls
+    标签格式:[batch_id, class_id, x, y, w, h, conf] x M, M为整个batch总的预测框数量
+    """
     # Convert model output to target format [batch_id, class_id, x, y, w, h, conf]
     targets = []
     for i, o in enumerate(output):
@@ -123,8 +149,16 @@ def output_to_target(output):
 
 
 def plot_images(images, targets, paths=None, fname='images.jpg', names=None, max_size=640, max_subplots=16):
+    """可视化训练/测试数据
+    :param images：一个batch的图片
+    :param labels：一个batch的标签
+    :param paths：一个batch的文件名
+    :param fname：保存可视化之后大图的文件路径
+    :param names：类别名
+    :param max_size：限制每张可视化图片的最大图片大小
+    :param max_subplots：最多可视化batch-size=16张图片
+    """
     # Plot image grid with labels
-
     if isinstance(images, torch.Tensor):
         images = images.cpu().float().numpy()
     if isinstance(targets, torch.Tensor):
@@ -134,64 +168,83 @@ def plot_images(images, targets, paths=None, fname='images.jpg', names=None, max
     if np.max(images[0]) <= 1:
         images *= 255
 
+    # 设置线宽度，字体大小
     tl = 3  # line thickness
     tf = max(tl - 1, 1)  # font thickness
     bs, _, h, w = images.shape  # batch size, _, height, width
     bs = min(bs, max_subplots)  # limit plot images
+    # 子画布的数量, 总数量为ns * ns
     ns = np.ceil(bs ** 0.5)  # number of subplots (square)
 
     # Check if we should resize
+    # 图片超过设定的最大size，要进行resize
     scale_factor = max_size / max(h, w)
     if scale_factor < 1:
         h = math.ceil(scale_factor * h)
         w = math.ceil(scale_factor * w)
 
+    # 初始化大图
     mosaic = np.full((int(ns * h), int(ns * w), 3), 255, dtype=np.uint8)  # init
     for i, img in enumerate(images):
         if i == max_subplots:  # if last batch has fewer images than we expect
             break
 
+        # 获取每张小图在整张大图上的左上角位置
         block_x = int(w * (i // ns))
         block_y = int(h * (i % ns))
 
+        # CHW -> HWC
         img = img.transpose(1, 2, 0)
+        # resize图片
         if scale_factor < 1:
             img = cv2.resize(img, (w, h))
 
+        # 将小图放在大图对应位置上
         mosaic[block_y:block_y + h, block_x:block_x + w, :] = img
         if len(targets) > 0:
+            # 获取当前batch_i的标签
             image_targets = targets[targets[:, 0] == i]
             boxes = xywh2xyxy(image_targets[:, 2:6]).T
             classes = image_targets[:, 1].astype('int')
+            # image_targets.shape[1] == 6时，表示batch_id, cls,xyxy标签,可视化的是标签，不是预测框
             labels = image_targets.shape[1] == 6  # labels if no conf column
             conf = None if labels else image_targets[:, 6]  # check for confidence presence (label vs pred)
 
             if boxes.shape[1]:
+                # 如果边框是归一化了的，则放大到基于原图
                 if boxes.max() <= 1.01:  # if normalized with tolerance 0.01
                     boxes[[0, 2]] *= w  # scale to pixels
                     boxes[[1, 3]] *= h
+                # 如果不是归一化的，且之前图片进行缩放了，则乘以scale_factor
                 elif scale_factor < 1:  # absolute coords need scale if image scales
                     boxes *= scale_factor
+            # 标签边框加上 相对于大图左上角的坐标
             boxes[[0, 2]] += block_x
             boxes[[1, 3]] += block_y
+            # 在子图上画框
             for j, box in enumerate(boxes.T):
+                # 获取类别，画框颜色，类别名字
                 cls = int(classes[j])
                 color = colors(cls)
                 cls = names[cls] if names else cls
+                # 如果是画预测框，则只画conf > 0.25的, 否则当置信度阈值设置较低时，会有太多的框
                 if labels or conf[j] > 0.25:  # 0.25 conf thresh
                     label = '%s' % cls if labels else '%s %.1f' % (cls, conf[j])
                     plot_one_box(box, mosaic, label=label, color=color, line_thickness=tl)
 
         # Draw image filename labels
         if paths:
+            # 大图左上角显示上图片路径名, 长度不超过40
             label = Path(paths[i]).name[:40]  # trim to 40 char
             t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
             cv2.putText(mosaic, label, (block_x + 5, block_y + t_size[1] + 5), 0, tl / 3, [220, 220, 220], thickness=tf,
                         lineType=cv2.LINE_AA)
 
         # Image border
+        # 画每个子图的边界
         cv2.rectangle(mosaic, (block_x, block_y), (block_x + w, block_y + h), (255, 255, 255), thickness=3)
 
+    # 保存图片
     if fname:
         r = min(1280. / max(h, w) / ns, 1.0)  # ratio to limit image size
         mosaic = cv2.resize(mosaic, (int(ns * w * r), int(ns * h * r)), interpolation=cv2.INTER_AREA)
@@ -201,6 +254,7 @@ def plot_images(images, targets, paths=None, fname='images.jpg', names=None, max
 
 
 def plot_lr_scheduler(optimizer, scheduler, epochs=300, save_dir=''):
+    """画学习率衰减图"""
     # Plot LR simulating training for full epochs
     optimizer, scheduler = copy(optimizer), copy(scheduler)  # do not modify originals
     y = []
@@ -218,6 +272,7 @@ def plot_lr_scheduler(optimizer, scheduler, epochs=300, save_dir=''):
 
 
 def plot_val_txt():  # from utils.plots import *; plot_val()
+    """画val.txt"""
     # Plot val.txt histograms
     x = np.loadtxt('val.txt', dtype=np.float32)
     box = xyxy2xywh(x[:, :4])
@@ -235,6 +290,7 @@ def plot_val_txt():  # from utils.plots import *; plot_val()
 
 
 def plot_targets_txt():  # from utils.plots import *; plot_targets_txt()
+    """画targets.txt"""
     # Plot targets.txt histograms
     x = np.loadtxt('targets.txt', dtype=np.float32).T
     s = ['x targets', 'y targets', 'width targets', 'height targets']
@@ -248,6 +304,7 @@ def plot_targets_txt():  # from utils.plots import *; plot_targets_txt()
 
 
 def plot_study_txt(path='', x=None):  # from utils.plots import *; plot_study_txt()
+    """通过val.py画yolov5学习曲线，也就是README.md中的曲线图"""
     # Plot study.txt generated by val.py
     plot2 = False  # plot additional results
     if plot2:
@@ -282,6 +339,7 @@ def plot_study_txt(path='', x=None):  # from utils.plots import *; plot_study_tx
 
 
 def plot_labels(labels, names=(), save_dir=Path('')):
+    """画标签的类别数量分布，边框分布，中心点，宽高分布"""
     # plot dataset labels
     print('Plotting labels... ')
     c, b = labels[:, 0], labels[:, 1:].transpose()  # classes, boxes
@@ -295,22 +353,28 @@ def plot_labels(labels, names=(), save_dir=Path('')):
 
     # matplotlib labels
     matplotlib.use('svg')  # faster
+    # 初始化画布
     ax = plt.subplots(2, 2, figsize=(8, 8), tight_layout=True)[1].ravel()
+    # 画类别数量分布
     y = ax[0].hist(c, bins=np.linspace(0, nc, nc + 1) - 0.5, rwidth=0.8)
     # [y[2].patches[i].set_color([x / 255 for x in colors(i)]) for i in range(nc)]  # update colors bug #3195
     ax[0].set_ylabel('instances')
     if 0 < len(names) < 30:
+        # 类别数小于30则显示类别名
         ax[0].set_xticks(range(len(names)))
         ax[0].set_xticklabels(names, rotation=90, fontsize=10)
     else:
         ax[0].set_xlabel('classes')
+    # 中心点分布，宽高分布
     sn.histplot(x, x='x', y='y', ax=ax[2], bins=50, pmax=0.9)
     sn.histplot(x, x='width', y='height', ax=ax[3], bins=50, pmax=0.9)
 
     # rectangles
+    # 固定中心点，xywh -> xyxy
     labels[:, 1:3] = 0.5  # center
     labels[:, 1:] = xywh2xyxy(labels[:, 1:]) * 2000
     img = Image.fromarray(np.ones((2000, 2000, 3), dtype=np.uint8) * 255)
+    # 画标签框大小分布，只画前1000个
     for cls, *box in labels[:1000]:
         ImageDraw.Draw(img).rectangle(box, width=1, outline=colors(cls))  # plot
     ax[1].imshow(img)
@@ -382,6 +446,9 @@ def plot_evolve(evolve_csv=Path('path/to/evolve.csv')):  # from utils.plots impo
 
 
 def plot_results(file='path/to/results.csv', dir=''):
+    """根据训练时保存的result.txt绘制result.png可视化
+    start/stop：表示绘制范围，从某个epoch到某个epoch
+    """
     # Plot training results.csv. Usage: from utils.plots import *; plot_results('path/to/results.csv')
     save_dir = Path(file).parent if file else Path(dir)
     fig, ax = plt.subplots(2, 5, figsize=(12, 6), tight_layout=True)
@@ -390,9 +457,11 @@ def plot_results(file='path/to/results.csv', dir=''):
     assert len(files), f'No results.csv files found in {save_dir.resolve()}, nothing to plot.'
     for fi, f in enumerate(files):
         try:
+            # 选取result.txt中需要绘制的数据
             data = pd.read_csv(f)
             s = [x.strip() for x in data.columns]
             x = data.values[:, 0]
+            # 如果损失为nan，则不绘制该epoch的损失
             for i, j in enumerate([1, 2, 3, 4, 5, 8, 9, 10, 6, 7]):
                 y = data.values[:, j]
                 # y[y == 0] = np.nan  # don't show zero values
@@ -407,23 +476,26 @@ def plot_results(file='path/to/results.csv', dir=''):
 
 
 def feature_visualization(x, module_type, stage, n=32, save_dir=Path('runs/detect/exp')):
+    """可视化每层网络的输出"""
     """
-    x:              Features to be visualized
-    module_type:    Module type
+    x:              Features to be visualized, 网络中间层输出
+    module_type:    Module type,模型类别
     stage:          Module stage within model
     n:              Maximum number of feature maps to plot
-    save_dir:       Directory to save results
+    save_dir:       Directory to save results, 最多可视化多少个channel
     """
     if 'Detect' not in module_type:
         batch, channels, height, width = x.shape  # batch, channels, height, width
         if height > 1 and width > 1:
             f = f"stage{stage}_{module_type.split('.')[-1]}_features.png"  # filename
 
+            # torch.chunk将x[0], 在channel维度，分割成channels个块，返回一个tuple
             blocks = torch.chunk(x[0].cpu(), channels, dim=0)  # select batch index 0, block by channels
             n = min(n, channels)  # number of plots
             fig, ax = plt.subplots(math.ceil(n / 8), 8, tight_layout=True)  # 8 rows x n/8 cols
             ax = ax.ravel()
             plt.subplots_adjust(wspace=0.05, hspace=0.05)
+            # 绘制每个通道特征图
             for i in range(n):
                 ax[i].imshow(blocks[i].squeeze())  # cmap='gray'
                 ax[i].axis('off')
