ccd99662147e545a284f046f1c4d9c2f9fb67c1c
杂杳 <2267379130@qq.com>
2021-10-01 17:06:49 +0800

    1.update train.py val.py 2.add detect.py chinese annations

--- YOLO/yolov3_master/detect.py
+++ YOLO/yolov3_master/detect.py
@@ -22,31 +22,42 @@ def detect(opt):
         ('rtsp://', 'rtmp://', 'http://', 'https://'))
 
     # Directories
+    # 获取保存预测路径
     save_dir = increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok)  # increment run
     (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
 
     # Initialize
+    # 初始化logging
     set_logging()
     device = select_device(opt.device)
     half = device.type != 'cpu'  # half precision only supported on CUDA
 
     # Load model
+    # 加载Float32模型，确保用户设定的输入图片分辨率能整除32(如不能则调整为能整除并返回)
     model = attempt_load(weights, map_location=device)  # load FP32 model
     stride = int(model.stride.max())  # model stride
     imgsz = check_img_size(imgsz, s=stride)  # check img_size
+    # 获取类别名字
     names = model.module.names if hasattr(model, 'module') else model.names  # get class names
     if half:
+        # 设置Float16
         model.half()  # to FP16
 
     # Second-stage classifier
     classify = False
     if classify:
+        # 设置第二次分类，默认设置为False不做使用
         modelc = load_classifier(name='resnet101', n=2)  # initialize
         modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
 
     # Set Dataloader
+    '''
+    设置数据加载器
+    '''
     vid_path, vid_writer = None, None
-    if webcam:
+    # 通过不同的输入源来设置不同的数据加载方式
+    if webcam: # 网络摄像头
+        # 检查当前环境是否能够正常imshow
         view_img = check_imshow()
         cudnn.benchmark = True  # set True to speed up constant image size inference
         dataset = LoadStreams(source, img_size=imgsz, stride=stride)
@@ -54,9 +65,17 @@ def detect(opt):
         dataset = LoadImages(source, img_size=imgsz, stride=stride)
 
     # Run inference
+    # 进行一次前向推理,测试程序是否正常
     if device.type != 'cpu':
         model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
     t0 = time.time()
+
+    """
+    path 图片/视频路径
+    img 进行resize+pad之后的图片
+    img0 原size图片
+    cap 当读取图片时为None，读取视频时为视频源
+    """
     for path, img, im0s, vid_cap in dataset:
         img = torch.from_numpy(img).to(device)
         img = img.half() if half else img.float()  # uint8 to fp16/32
@@ -66,63 +85,99 @@ def detect(opt):
 
         # Inference
         t1 = time_synchronized()
+        """
+        前向传播 返回pred的shape是(1, num_boxes, 5+num_class)
+        h,w为传入网络图片的长和宽，注意dataset在检测时使用了矩形推理，所以这里h不一定等于w
+        num_boxes = h/32 * w/32 + h/16 * w/16 + h/8 * w/8
+        pred[..., 0:4]为预测框坐标
+            预测框坐标为xywh(中心点+宽长)格式
+            pred[..., 4]为objectness置信度
+            pred[..., 5:-1]为分类结果
+        """
         pred = model(img, augment=opt.augment)[0]
 
         # Apply NMS
+        """
+        pred:前向传播的输出
+        conf_thres:置信度阈值
+        iou_thres:iou阈值
+        classes:是否只保留特定的类别
+        agnostic_nms:进行nms是否也去除不同类别之间的框
+        max-det:保留的最大检测框数量
+        经过nms之后，预测框格式：xywh-->xyxy(左上角右下角)
+        pred是一个列表list[torch.tensor]，长度为batch_size
+        每一个torch.tensor的shape为(num_boxes, 6),内容为box+conf+cls
+        """
         pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, opt.classes, opt.agnostic_nms,
                                    max_det=opt.max_det)
         t2 = time_synchronized()
 
         # Apply Classifier
+        # 应用二次分类(默认设置为False)
         if classify:
             pred = apply_classifier(pred, modelc, img, im0s)
 
         # Process detections
+        # 对每一张图片作处理
         for i, det in enumerate(pred):  # detections per image
+            # 如果输入源是webcam，则batch_size不为1，取出dataset中的一张图片
             if webcam:  # batch_size >= 1
                 p, s, im0, frame = path[i], f'{i}: ', im0s[i].copy(), dataset.count
             else:
                 p, s, im0, frame = path, '', im0s.copy(), getattr(dataset, 'frame', 0)
 
             p = Path(p)  # to Path
+            # 设置保存图片/视频的路径
             save_path = str(save_dir / p.name)  # img.jpg
+            # 设置保存框坐标txt文件的路径
             txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
+            # 设置打印信息(图片长宽)
             s += '%gx%g ' % img.shape[2:]  # print string
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
             imc = im0.copy() if opt.save_crop else im0  # for opt.save_crop
             if len(det):
                 # Rescale boxes from img_size to im0 size
+                # 调整预测框的坐标：基于resize+pad的图片的坐标-->基于原size图片的坐标
+                # 此时坐标格式为xyxy
                 det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
 
                 # Print results
+                # 打印检测到的类别数量
                 for c in det[:, -1].unique():
                     n = (det[:, -1] == c).sum()  # detections per class
                     s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
 
                 # Write results
+                # 保存预测结果
                 for *xyxy, conf, cls in reversed(det):
                     if save_txt:  # Write to file
+                        # 将xyxy(左上角+右下角)格式转为xywh(中心点+宽长)格式，并除上w，h做归一化，转化为列表再保存
                         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
                         line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
                         with open(txt_path + '.txt', 'a') as f:
                             f.write(('%g ' * len(line)).rstrip() % line + '\n')
 
+                    # 在原图上画框
                     if save_img or opt.save_crop or view_img:  # Add bbox to image
                         c = int(cls)  # integer class
                         label = None if opt.hide_labels else (names[c] if opt.hide_conf else f'{names[c]} {conf:.2f}')
                         plot_one_box(xyxy, im0, label=label, color=colors(c, True), line_thickness=opt.line_thickness)
                         if opt.save_crop:
+                            # 保存crop
                             save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
 
             # Print time (inference + NMS)
+            # 打印前向传播+nms时间
             print(f'{s}Done. ({t2 - t1:.3f}s)')
 
             # Stream results
+            # 如果设置展示，则show图片/视频
             if view_img:
                 cv2.imshow(str(p), im0)
                 cv2.waitKey(1)  # 1 millisecond
 
             # Save results (image with detections)
+            # 设置保存图片/视频
             if save_img:
                 if dataset.mode == 'image':
                     cv2.imwrite(save_path, im0)
@@ -150,27 +205,49 @@ def detect(opt):
 
 def detect_parse_opt():
     parser = argparse.ArgumentParser()
+    # weights:训练的权重
     parser.add_argument('--weights', nargs='+', type=str, default='yolov3.pt', help='model.pt path(s)')
+    # source:测试数据，可以是图片/视频路径，也可以是'0'(电脑自带摄像头),也可以是rtsp等视频流
     parser.add_argument('--source', type=str, default='data/images', help='source')  # file/folder, 0 for webcam
+    # imgsz:网络输入图片大小
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    # conf-thres:置信度阈值
     parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
+    # iou - thres: 做nms的iou阈值
     parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+    # max-det:保留的最大检测框数量
     parser.add_argument('--max-det', type=int, default=1000, help='maximum number of detections per image')
+    # device:设置设备
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    # view-img:是否展示预测之后的图片/视频，默认False
     parser.add_argument('--view-img', action='store_true', help='display results')
+    # view-img:是否展示预测之后的图片/视频，默认False
     parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    # save-conf:是否将置信度conf也保存到txt中，默认False
     parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    # save-crop:是否保存裁剪预测框图片
     parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
+    # nosave:不保存图片、视频
     parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+    # classes:设置只保留某一部分类别，形如0或者0 2 3
     parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+    # agnostic-nms: 进行nms是否也去除不同类别之间的框，默认False
     parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    # augment:推理的时候进行多尺度，翻转等操作(TTA)推理
     parser.add_argument('--augment', action='store_true', help='augmented inference')
+    # update:如果为True，则对所有模型进行strip_optimizer操作，去除pt文件中的优化器等信息，默认为False
     parser.add_argument('--update', action='store_true', help='update all models')
+    # project:保存测试日志的文件夹路径
     parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+    # name:保存测试日志文件夹的名字, 所以最终是保存在project/name中
     parser.add_argument('--name', default='exp', help='save results to project/name')
+    # exist_ok: 是否重新创建日志文件, False时重新创建文件
     parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    # line - thickness: 画框的线条粗细
     parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
+    # hide-labels:可视化时隐藏预测类别
     parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
+    # hide-conf:可视化时隐藏置信度
     parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
     opt = parser.parse_args()
     print(opt)
--- YOLO/yolov3_master/val.py
+++ YOLO/yolov3_master/val.py
@@ -287,7 +287,7 @@ def test(data,
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(prog='test.py')
+    parser = argparse.ArgumentParser(prog='val.py')
     parser.add_argument('--weights', nargs='+', type=str, default='yolov3.pt', help='model.pt path(s)')
     parser.add_argument('--data', type=str, default='data/coco128.yaml', help='*.data path')
     parser.add_argument('--batch-size', type=int, default=32, help='size of each image batch')
@@ -334,7 +334,7 @@ if __name__ == '__main__':
             test(opt.data, w, opt.batch_size, opt.img_size, 0.25, 0.45, save_json=False, plots=False, opt=opt)
 
     elif opt.task == 'study':  # run over a range of settings and save/plot
-        # python test.py --task study --data coco.yaml --iou 0.7 --weights yolov3.pt yolov3-spp.pt yolov3-tiny.pt
+        # python val.py --task study --data coco.yaml --iou 0.7 --weights yolov3.pt yolov3-spp.pt yolov3-tiny.pt
         x = list(range(256, 1536 + 128, 128))  # x axis (image sizes)
         for w in opt.weights:
             f = f'study_{Path(opt.data).stem}_{Path(w).stem}.txt'  # filename to save to
