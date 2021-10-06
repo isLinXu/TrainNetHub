b2be2d3ee31a5f4adf695239a00e74bc630d9a09
杂杳 <2267379130@qq.com>
2021-10-06 17:48:31 +0800

    1.add annations loss.py 2.add annations metrics.py 3.add plots.py

--- YOLO/yolov3_master/utils/metrics.py
+++ YOLO/yolov3_master/utils/metrics.py
@@ -10,12 +10,20 @@ from . import general
 
 
 def fitness(x):
+    """根据模型指标P, R, mAP@0.5, mAP@0.5:0.95，返回一个综合分数,用来选取best.pt"""
     # Model fitness as a weighted combination of metrics
     w = [0.0, 0.0, 0.1, 0.9]  # weights for [P, R, mAP@0.5, mAP@0.5:0.95]
     return (x[:, :4] * w).sum(1)
 
 
 def ap_per_class(tp, conf, pred_cls, target_cls, plot=False, save_dir='.', names=()):
+    """计算每个类别的ap
+    :param tp:根据iou阈值计算的true positive, ndarray, [n, 10],
+                10表示range[0.5, 0.95],间隔0.05取一个iou阈值,预测与标签超过这个iou阈值才为tp
+    :param conf:置信度，ndarray, [n, 1]
+    :param pred_cls:预测类别，ndarray, [n, 1]
+    :param plot:是否画mAP@0.5的PR曲线
+    """
     """ Compute the average precision, given the recall and precision curves.
     Source: https://github.com/rafaelpadilla/Object-Detection-Metrics.
     # Arguments
@@ -30,44 +38,61 @@ def ap_per_class(tp, conf, pred_cls, target_cls, plot=False, save_dir='.', names
     """
 
     # Sort by objectness
+    # 将tp，conf，pred_cls按照置信度从大到小排序
     i = np.argsort(-conf)
     tp, conf, pred_cls = tp[i], conf[i], pred_cls[i]
 
     # Find unique classes
+    # 将target_cls去重，获得类别
     unique_classes = np.unique(target_cls)
+    # 获得类别数
     nc = unique_classes.shape[0]  # number of classes, number of detections
 
     # Create Precision-Recall curve and compute AP for each class
+    # 初始化坐标x,y
     px, py = np.linspace(0, 1, 1000), []  # for plotting
+    # 初始化指标，ap，precision，recall
     ap, p, r = np.zeros((nc, tp.shape[1])), np.zeros((nc, 1000)), np.zeros((nc, 1000))
+
+    # 对每个类别处理
     for ci, c in enumerate(unique_classes):
+        # 选取类别为c的索引
         i = pred_cls == c
+        # c类别标签的数量
         n_l = (target_cls == c).sum()  # number of labels
+        # c类别预测的数量
         n_p = i.sum()  # number of predictions
 
         if n_p == 0 or n_l == 0:
             continue
         else:
             # Accumulate FPs and TPs
+            # 累计计算fp，tp
             fpc = (1 - tp[i]).cumsum(0)
             tpc = tp[i].cumsum(0)
 
             # Recall
+            # 计算recall
             recall = tpc / (n_l + 1e-16)  # recall curve
             r[ci] = np.interp(-px, -conf[i], recall[:, 0], left=0)  # negative x, xp because xp decreases
 
             # Precision
+            # 计算precision
             precision = tpc / (tpc + fpc)  # precision curve
+            # 插值，方便绘制基于iou_thres=0.5的准确率曲线
             p[ci] = np.interp(-px, -conf[i], precision[:, 0], left=1)  # p at pr_score
 
             # AP from recall-precision curve
+            # 根据precision与recall计算ap
             for j in range(tp.shape[1]):
                 ap[ci, j], mpre, mrec = compute_ap(recall[:, j], precision[:, j])
                 if plot and j == 0:
                     py.append(np.interp(px, mrec, mpre))  # precision at mAP@0.5
 
     # Compute F1 (harmonic mean of precision and recall)
+    # 根据precision与recall计算f1值
     f1 = 2 * p * r / (p + r + 1e-16)
+    # 画PR曲线，F1曲线，Precision, recall曲线(后三个的横坐标x为置信度)
     if plot:
         plot_pr_curve(px, py, ap, Path(save_dir) / 'PR_curve.png', names)
         plot_mc_curve(px, f1, Path(save_dir) / 'F1_curve.png', names, ylabel='F1')
@@ -79,6 +104,7 @@ def ap_per_class(tp, conf, pred_cls, target_cls, plot=False, save_dir='.', names
 
 
 def compute_ap(recall, precision):
+    """根据precision与recall计算ap, 计算PR曲线下的面积"""
     """ Compute the average precision, given the recall and precision curves
     # Arguments
         recall:    The recall curve (list)
@@ -92,12 +118,14 @@ def compute_ap(recall, precision):
     mpre = np.concatenate(([1.], precision, [0.]))
 
     # Compute the precision envelope
+    # np.maximum.accumulate 计算数组的累计最大值
     mpre = np.flip(np.maximum.accumulate(np.flip(mpre)))
 
     # Integrate area under curve
     method = 'interp'  # methods: 'continuous', 'interp'
     if method == 'interp':
         x = np.linspace(0, 1, 101)  # 101-point interp (COCO)
+        # np.trapz求积分, 求得PR曲线下的面积
         ap = np.trapz(np.interp(x, mrec, mpre), x)  # integrate
     else:  # 'continuous'
         i = np.where(mrec[1:] != mrec[:-1])[0]  # points where x axis (recall) changes
@@ -107,6 +135,7 @@ def compute_ap(recall, precision):
 
 
 class ConfusionMatrix:
+    """计算预测的混淆矩阵"""
     # Updated version of https://github.com/kaanakan/object_detection_confusion_matrix
     def __init__(self, nc, conf=0.25, iou_thres=0.45):
         self.matrix = np.zeros((nc + 1, nc + 1))
@@ -124,33 +153,55 @@ class ConfusionMatrix:
         Returns:
             None, updates confusion matrix accordingly
         """
+        # 筛选大于置信度阈值的预测框
         detections = detections[detections[:, 4] > self.conf]
+        # 标签类别
         gt_classes = labels[:, 0].int()
+        # 预测类别
         detection_classes = detections[:, 5].int()
+        # 标签框与预测框的iou，(M, N)
         iou = general.box_iou(labels[:, 1:], detections[:, :4])
 
+        # 找到大于iou阈值的, x是一个包含两个元素的元组，
+        # 第一个是满足条件的行索引, 第二个是满足条件的列索引，长度为iou中大于iou阈值的个数n
         x = torch.where(iou > self.iou_thres)
         if x[0].shape[0]:
+            # torch.stack(x, 1).shape (n, 2), iou[x[0], x[1]][:, None].shape (n, 1)
+            # matches.shape (n, 3), 前两列为满足iou阈值的行索引和列索引, 第三列是该iou值
             matches = torch.cat((torch.stack(x, 1), iou[x[0], x[1]][:, None]), 1).cpu().numpy()
             if x[0].shape[0] > 1:
+                """因为np.unique去重机制是取先见到的元素，之后的重复元素去掉，
+                所以总的来说下面就是对每个标签框选其iou最大的预测框作为匹配
+                每个预测框也是选其iou最大的标签框作为匹配，留下的match都是相互iou最大的
+                """
+                # matches按iou大小 从大到小排序
                 matches = matches[matches[:, 2].argsort()[::-1]]
+                # 按照列索引去重之后的matches，
+                # np.unique(matches[:, 1], return_index=True)[1]返回的是列索引去重的索引
                 matches = matches[np.unique(matches[:, 1], return_index=True)[1]]
+                # matches按iou大小 从大到小排序
                 matches = matches[matches[:, 2].argsort()[::-1]]
+                # 按照行索引去重之后的matches，
                 matches = matches[np.unique(matches[:, 0], return_index=True)[1]]
         else:
             matches = np.zeros((0, 3))
 
         n = matches.shape[0] > 0
+        # m0, m1为行索引和列索引
         m0, m1, _ = matches.transpose().astype(np.int16)
         for i, gc in enumerate(gt_classes):
+            # 选取该match的标签框类别为gc的
             j = m0 == i
+            # 如果有m0中有索引i的对应，则对应混淆矩阵位置的值+1
             if n and sum(j) == 1:
                 self.matrix[detection_classes[m1[j]], gc] += 1  # correct
+            # 否则就是目标漏检+1, 也就是将目标错误检测成了背景, 背景的误检
             else:
                 self.matrix[self.nc, gc] += 1  # background FP
 
         if n:
             for i, dc in enumerate(detection_classes):
+                # 如果m1中没有索引i的对应，则目标误检+1, 也就是将背景错误检测成了目标, 背景的漏检
                 if not any(m1 == i):
                     self.matrix[dc, self.nc] += 1  # background FN
 
@@ -158,9 +209,10 @@ class ConfusionMatrix:
         return self.matrix
 
     def plot(self, save_dir='', names=()):
+        """绘制混淆矩阵"""
         try:
             import seaborn as sn
-
+            # 以概率的形式显示矩阵
             array = self.matrix / (self.matrix.sum(0).reshape(1, self.nc + 1) + 1E-6)  # normalize
             array[array < 0.005] = np.nan  # don't annotate (would appear as 0.00)
 
@@ -177,6 +229,7 @@ class ConfusionMatrix:
             pass
 
     def print(self):
+        """打印矩阵"""
         for i in range(self.nc + 1):
             print(' '.join(map(str, self.matrix[i])))
 
@@ -184,6 +237,7 @@ class ConfusionMatrix:
 # Plots ----------------------------------------------------------------------------------------------------------------
 
 def plot_pr_curve(px, py, ap, save_dir='pr_curve.png', names=()):
+    """画PR曲线"""
     # Precision-recall curve
     fig, ax = plt.subplots(1, 1, figsize=(9, 6), tight_layout=True)
     py = np.stack(py, axis=1)
@@ -192,8 +246,10 @@ def plot_pr_curve(px, py, ap, save_dir='pr_curve.png', names=()):
         for i, y in enumerate(py.T):
             ax.plot(px, y, linewidth=1, label=f'{names[i]} {ap[i, 0]:.3f}')  # plot(recall, precision)
     else:
+        # 仅画PR曲线
         ax.plot(px, py, linewidth=1, color='grey')  # plot(recall, precision)
 
+    # 画所有类别的综合PR曲线, 并显示map
     ax.plot(px, py.mean(1), linewidth=3, color='blue', label='all classes %.3f mAP@0.5' % ap[:, 0].mean())
     ax.set_xlabel('Recall')
     ax.set_ylabel('Precision')
@@ -204,6 +260,7 @@ def plot_pr_curve(px, py, ap, save_dir='pr_curve.png', names=()):
 
 
 def plot_mc_curve(px, py, save_dir='mc_curve.png', names=(), xlabel='Confidence', ylabel='Metric'):
+    """以置信度为横坐标画曲线"""
     # Metric-confidence curve
     fig, ax = plt.subplots(1, 1, figsize=(9, 6), tight_layout=True)
 
--- YOLO/yolov3_master/utils/plots.py
+++ YOLO/yolov3_master/utils/plots.py
@@ -29,17 +29,20 @@ class Colors:
     # Ultralytics color palette https://ultralytics.com/
     def __init__(self):
         # hex = matplotlib.colors.TABLEAU_COLORS.values()
+        # 十六进制格式颜色
         hex = ('FF3838', 'FF9D97', 'FF701F', 'FFB21D', 'CFD231', '48F90A', '92CC17', '3DDB86', '1A9334', '00D4BB',
                '2C99A8', '00C2FF', '344593', '6473FF', '0018EC', '8438FF', '520085', 'CB38FF', 'FF95C8', 'FF37C7')
         self.palette = [self.hex2rgb('#' + c) for c in hex]
         self.n = len(self.palette)
 
     def __call__(self, i, bgr=False):
+        # 返回对应颜色
         c = self.palette[int(i) % self.n]
         return (c[2], c[1], c[0]) if bgr else c
 
     @staticmethod
     def hex2rgb(h):  # rgb order (PIL)
+        # 16进制的颜色格式转为RGB格式
         return tuple(int(h[1 + i:1 + i + 2], 16) for i in (0, 2, 4))
 
 
@@ -47,6 +50,7 @@ colors = Colors()  # create instance for 'from utils.plots import colors'
 
 
 def hist2d(x, y, n=100):
+    '''用于绘制labels.png 和evolve.png 的二维直方图'''
     # 2d histogram used in labels.png and evolve.png
     xedges, yedges = np.linspace(x.min(), x.max(), n), np.linspace(y.min(), y.max(), n)
     hist, xedges, yedges = np.histogram2d(x, y, (xedges, yedges))
@@ -69,20 +73,47 @@ def butter_lowpass_filtfilt(data, cutoff=1500, fs=50000, order=5):
 
 
 def plot_one_box(x, im, color=(128, 128, 128), label=None, line_thickness=3):
+    """一般会用在detect.py中在nms之后变量每一个预测框，再将每个预测框画在原图上
+       使用opencv在原图im上画一个bounding box
+       :params x: 预测得到的bounding box  [x1 y1 x2 y2]
+       :params im: 原图 要将bounding box画在这个图上  array
+       :params color: bounding box线的颜色
+       :params labels: 标签上的框框信息  类别 + score
+       :params line_thickness: bounding box的线宽
+       """
     # Plots one bounding box on image 'im' using OpenCV
+    # check im内存是否连续
     assert im.data.contiguous, 'Image not contiguous. Apply np.ascontiguousarray(im) to plot_on_box() input image.'
+    # tl = 框框的线宽  要么等于line_thickness要么根据原图im长宽信息自适应生成一个
     tl = line_thickness or round(0.002 * (im.shape[0] + im.shape[1]) / 2) + 1  # line/font thickness
+    # c1 = (x1, y1) = 矩形框的左上角   c2 = (x2, y2) = 矩形框的右下角
     c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
+
+    # cv2.rectangle: 在im上画出框框   c1: start_point(x1, y1)  c2: end_point(x2, y2)
+    # 注意: 这里的c1+c2可以是左上角+右下角  也可以是左下角+右上角都可以
     cv2.rectangle(im, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+
+    # 如果label不为空还要在框框上面显示标签label + score
+    # label结构为name, conf.
     if label:
+        # label字体的线宽 font thickness
         tf = max(tl - 1, 1)  # font thickness
+
+        # cv2.getTextSize: 根据输入的label信息计算文本字符串的宽度和高度
+        # 0: 文字字体类型  fontScale: 字体缩放系数  thickness: 字体笔画线宽
+        # 返回retval 字体的宽高 (width, height), baseLine 相对于最底端文本的 y 坐标
         t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
         c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+        # 将框体的上面矩形都填充color颜色
         cv2.rectangle(im, c1, c2, color, -1, cv2.LINE_AA)  # filled
         cv2.putText(im, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
 
 
 def plot_one_box_PIL(box, im, color=(128, 128, 128), label=None, line_thickness=None):
+    """使用Pillow画单个框
+       :param x: 坐标，xyxy格式左上角右下角
+       :param im:原图
+    """
     # Plots one bounding box on image 'im' using PIL
     im = Image.fromarray(im)
     draw = ImageDraw.Draw(im)
@@ -97,6 +128,7 @@ def plot_one_box_PIL(box, im, color=(128, 128, 128), label=None, line_thickness=
 
 
 def plot_wh_methods():  # from utils.plots import *; plot_wh_methods()
+    """画yolov3/yolov5坐标回归的范围"""
     # Compares the two methods for width-height anchor multiplication
     # https://github.com/ultralytics/yolov3/issues/168
     x = np.arange(-4.0, 4.0, .1)
@@ -117,6 +149,10 @@ def plot_wh_methods():  # from utils.plots import *; plot_wh_methods()
 
 
 def output_to_target(output):
+    """将预测输出格式 转化为 标签格式，test.py中可视化预测时使用
+       网络输出格式为:[(N, 6), ] x batch, xyxy, conf, cls
+       标签格式:[batch_id, class_id, x, y, w, h, conf] x M, M为整个batch总的预测框数量
+       """
     # Convert model output to target format [batch_id, class_id, x, y, w, h, conf]
     targets = []
     for i, o in enumerate(output):
@@ -126,6 +162,15 @@ def output_to_target(output):
 
 
 def plot_images(images, targets, paths=None, fname='images.jpg', names=None, max_size=640, max_subplots=16):
+    """可视化训练/测试数据
+    :param images：一个batch的图片
+    :param labels：一个batch的标签
+    :param paths：一个batch的文件名
+    :param fname：保存可视化之后大图的文件路径
+    :param names：类别名
+    :param max_size：限制每张可视化图片的最大图片大小
+    :param max_subplots：最多可视化batch-size=16张图片
+    """
     # Plot image grid with labels
 
     if isinstance(images, torch.Tensor):
@@ -170,6 +215,7 @@ def plot_images(images, targets, paths=None, fname='images.jpg', names=None, max
             conf = None if labels else image_targets[:, 6]  # check for confidence presence (label vs pred)
 
             if boxes.shape[1]:
+                # 如果边框是归一化了的，则放大到基于原图
                 if boxes.max() <= 1.01:  # if normalized with tolerance 0.01
                     boxes[[0, 2]] *= w  # scale to pixels
                     boxes[[1, 3]] *= h
@@ -221,6 +267,7 @@ def plot_lr_scheduler(optimizer, scheduler, epochs=300, save_dir=''):
 
 
 def plot_test_txt():  # from utils.plots import *; plot_test()
+    """画val.txt"""
     # Plot test.txt histograms
     x = np.loadtxt('test.txt', dtype=np.float32)
     box = xyxy2xywh(x[:, :4])
@@ -238,6 +285,7 @@ def plot_test_txt():  # from utils.plots import *; plot_test()
 
 
 def plot_targets_txt():  # from utils.plots import *; plot_targets_txt()
+    """画targets.txt"""
     # Plot targets.txt histograms
     x = np.loadtxt('targets.txt', dtype=np.float32).T
     s = ['x targets', 'y targets', 'width targets', 'height targets']
@@ -251,6 +299,12 @@ def plot_targets_txt():  # from utils.plots import *; plot_targets_txt()
 
 
 def plot_study_txt(path='', x=None):  # from utils.plots import *; plot_study_txt()
+    '''
+    绘制由val.py生成的 study.txt
+    :param path:
+    :param x:
+    :return:
+    '''
     # Plot study.txt generated by val.py
     fig, ax = plt.subplots(2, 4, figsize=(10, 6), tight_layout=True)
     # ax = ax.ravel()
@@ -283,6 +337,7 @@ def plot_study_txt(path='', x=None):  # from utils.plots import *; plot_study_tx
 
 
 def plot_labels(labels, names=(), save_dir=Path(''), loggers=None):
+    """画标签的类别数量分布，边框分布，中心点，宽高分布"""
     # plot dataset labels
     print('Plotting labels... ')
     c, b = labels[:, 0], labels[:, 1:].transpose()  # classes, boxes
@@ -296,22 +351,28 @@ def plot_labels(labels, names=(), save_dir=Path(''), loggers=None):
 
     # matplotlib labels
     matplotlib.use('svg')  # faster
+    # 初始化画布
     ax = plt.subplots(2, 2, figsize=(8, 8), tight_layout=True)[1].ravel()
+    # 画类别数量分布
     y = ax[0].hist(c, bins=np.linspace(0, nc, nc + 1) - 0.5, rwidth=0.8)
     # [y[2].patches[i].set_color([x / 255 for x in colors(i)]) for i in range(nc)]  # update colors bug #3195 
     ax[0].set_ylabel('instances')
     if 0 < len(names) < 30:
+        # 类别数小于30则显示类别名
         ax[0].set_xticks(range(len(names)))
         ax[0].set_xticklabels(names, rotation=90, fontsize=10)
     else:
         ax[0].set_xlabel('classes')
+    # 中心点分布，宽高分布
     sns.histplot(x, x='x', y='y', ax=ax[2], bins=50, pmax=0.9)
     sns.histplot(x, x='width', y='height', ax=ax[3], bins=50, pmax=0.9)
 
     # rectangles
+    # 固定中心点，xywh -> xyxy
     labels[:, 1:3] = 0.5  # center
     labels[:, 1:] = xywh2xyxy(labels[:, 1:]) * 2000
     img = Image.fromarray(np.ones((2000, 2000, 3), dtype=np.uint8) * 255)
+    # 画标签框大小分布，只画前1000个
     for cls, *box in labels[:1000]:
         ImageDraw.Draw(img).rectangle(box, width=1, outline=colors(cls))  # plot
     ax[1].imshow(img)
@@ -411,6 +472,17 @@ def plot_results_overlay(start=0, stop=0):  # from utils.plots import *; plot_re
 
 
 def plot_results(start=0, stop=0, bucket='', id=(), labels=(), save_dir=''):
+    '''
+    根据训练时保存的result.txt绘制result.png可视化
+      start/stop：表示绘制范围，从某个epoch到某个epoch
+    :param start: 起点
+    :param stop:  终点
+    :param bucket: 分桶数量
+    :param id:     id
+    :param labels: 标签
+    :param save_dir: 保存路径
+    :return:
+    '''
     # Plot training 'results*.txt'. from utils.plots import *; plot_results(save_dir='runs/train/exp')
     fig, ax = plt.subplots(2, 5, figsize=(12, 6), tight_layout=True)
     ax = ax.ravel()
@@ -426,9 +498,11 @@ def plot_results(start=0, stop=0, bucket='', id=(), labels=(), save_dir=''):
     assert len(files), 'No results.txt files found in %s, nothing to plot.' % os.path.abspath(save_dir)
     for fi, f in enumerate(files):
         try:
+            # 选取result.txt中需要绘制的数据
             results = np.loadtxt(f, usecols=[2, 3, 4, 8, 9, 12, 13, 14, 10, 11], ndmin=2).T
             n = results.shape[1]  # number of rows
             x = range(start, min(stop, n) if stop else n)
+            # 如果损失为nan，则不绘制该epoch的损失
             for i in range(10):
                 y = results[i, x]
                 if i in [0, 1, 2, 5, 6, 7]:
