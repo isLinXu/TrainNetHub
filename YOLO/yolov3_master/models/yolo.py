cd21a43f9290c1a64486d297fa0cda69572c37e1
杂杳 <2267379130@qq.com>
2021-10-11 16:28:29 +0800

    1.move file

--- YOLO/yolov3_master/models/yolo.py
+++ YOLO/yolov3_master/models/yolo.py
@@ -13,11 +13,11 @@ from pathlib import Path
 sys.path.append(Path(__file__).parent.parent.absolute().__str__())  # to run '$ python *.py' files in subdirectories
 logger = logging.getLogger(__name__)
 
-from models.common import *
-from models.experimental import *
-from utils.autoanchor import check_anchor_order
-from utils.general import make_divisible, check_file, set_logging
-from utils.torch_utils import time_synchronized, fuse_conv_and_bn, model_info, scale_img, initialize_weights, \
+from YOLO.yolov3_master.models.common import *
+from YOLO.yolov3_master.models.experimental import *
+from YOLO.yolov3_master.utils.autoanchor import check_anchor_order
+from YOLO.yolov3_master.utils.general import make_divisible, check_file, set_logging
+from YOLO.yolov3_master.utils.torch_utils import time_synchronized, fuse_conv_and_bn, model_info, scale_img, initialize_weights, \
     select_device, copy_attr
 
 try:
