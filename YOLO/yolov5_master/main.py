a62162ce971db01740c642e1b964eee9dbdb227f
杂杳 <>
2022-01-31 19:24:07 +0800

    Transformer/SwinTransformer_Seg 1.add mmseg 2.add mmseg/apis 3.add mmseg/core 4.add mmseg/datasets 5.add mmseg/models 6.add mmseg/utils

--- YOLO/yolov5_master/main.py
+++ YOLO/yolov5_master/main.py
@@ -23,7 +23,6 @@ class PackageProjectUtil:
         if print_log: print(f'当前项目名称：{p_name}\r\n当前项目根路径：{root_path}')
         return root_path
 
-
 # 将当前项目目录添加至Python编译器路径(兼容python命令行运行方式)
 sys.path.append(PackageProjectUtil.project_root_path())
 # 当前目录
@@ -33,7 +32,7 @@ from YOLO.yolov5_master.train import train_main, train_parse_opt
 from YOLO.yolov5_master.detect import detect_main, detect_parse_opt
 
 
-def train_(object_name, models_name='yolov5s', continue_flag = False):
+def train_(object_name, models_name='yolov5s'):
     # 初始化参数列表
     t_opt = train_parse_opt()
     """
@@ -43,28 +42,22 @@ def train_(object_name, models_name='yolov5s', continue_flag = False):
     Usage-IDE使用方式：直接在下面对应位置进行修改
     """
     # 数据集配置文件
-    t_opt.data = rpath + '/data/' + 'custom/' + 'custom_' + object_name + '.yaml'
+    t_opt.data = rpath + '/data/' + 'custom/' + 'voc_' + object_name + '.yaml'
     # 模型配置文件
-    if continue_flag:
-        t_opt.cfg = rpath + '/models/custom/' + 'yolov5s' + '_' + object_name + '.yaml'
-    else:
-        t_opt.cfg = rpath + '/models/custom/' + models_name + '_' + object_name + '.yaml'
-
+    t_opt.cfg = rpath + '/models/custom/' + models_name + '_' + object_name + '.yaml'
     # 预训练权重
     # weights/yolov5l.pt,yolov5l6.pt,yolov5m.pt,yolov5m6.pt,yolov5s6.pt,yolov5x.pt,yolov5x6.pt
-    if continue_flag:
-        t_opt.weights = models_name
-    else:
-        t_opt.weights = rpath + '/weights/' + models_name + '.pt'
-
+    t_opt.weights = rpath + '/weights/' + models_name + '.pt'
     # 设置单次训练所选取的样本数
     t_opt.batch_size = 1
     # 设置训练样本训练的迭代次数
-    t_opt.epochs = 200
+    t_opt.epochs = 1024
     # 设置线程数
     t_opt.workers = 4
     # 训练结果的文件名称
     t_opt.name = models_name + '_' + object_name
+    # 是否单分类
+    t_opt.single_cls = True
 
     """开始训练"""
     train_main(t_opt)
@@ -81,15 +74,25 @@ def detect_(object_name, models_name='yolov5s'):
     """
     # 图像/图像集合/视频的源路径,内部自动文件类型进行判断
     # d_opt.source = rpath +'/data/images/bus.jpg'
-    d_opt.source = '/media/hxzh02/TU100Pro/Insulator/train/images/'
+    # d_opt.source = '/media/hxzh02/SB@home/hxzh/Dataset/11-5电塔照片视频/照片/'
+    # d_opt.source = '/media/hxzh02/SB@home/hxzh/Dataset/无人机相关数据集合集/3-输电线路异物数据集（VOC）/foreignbody_dataset_part1/images/val/'
+    # d_opt.source = '/home/hxzh02/MyGithub/TrainNetHub/YOLO/yolov5_master/data/VOCdevkit_tower_part/VOC2007/JPEGImages'
+    # d_opt.source = '/home/hxzh02/MyGithub/TrainNetHub/YOLO/yolov5_master/data/datasets_smoke/VOC2007/JPEGImages'
+    # d_opt.source = '/media/hxzh02/SB@home/hxzh/Dataset/无人机相关数据集合集/7-输电线路绝缘子数据集VOC/dataset_insulator/VOC2007/JPEGImages/'
+    # d_opt.source = '/media/hxzh02/SB@home/hxzh/Dataset/无人机相关数据集合集/5-安全帽数据集5000张/dataset_safetyHat/images/val/'
+
+    # d_opt.source = '/home/linxu/Desktop/data/VOCdevkit_towerlean_detect/VOC2007/JPEGImages/'
+    d_opt.source = '/home/linxu/Desktop/data/VOCdevkit_windbias_detect/VOC2007/JPEGImages/'
+    # d_opt.source = '/media/hxzh02/SB@home/hxzh/Dataset/输电杆塔照片素材'
+    # d_opt.source = '/home/hxzh02/文档/test_image/towerupdown'
     # 设置进行预测推理使用的权重模型文件
     # d_opt.weights = rpath + '/runs/train/' + models_name + '_' + object_name + '/weights/best.pt'
     # d_opt.weights = '/home/hxzh02/MyGithub/TrainNetHub/YOLO/yolov5_master/runs/train/yolov5s_tower4/weights/best.pt'
-    d_opt.weights = '/home/hxzh02/文档/plane-project_beijing/weight/insulator.pt'
-
     # /media/hxzh02/SB@home/hxzh/MyGithub/TrainNetHub/YOLO/yolov5_master/runs/train/yolov5s_tower_body2/weights/best.pt
     # d_opt.weights = '/media/hxzh02/SB@home/hxzh/MyGithub/TrainNetHub/YOLO/yolov5_master/runs/train/yolov5s_tower_only/weights/best.pt'
-    # d_opt.weights = '/media/hxzh02/SB@home/hxzh/MyGithub/TrainNetHub/YOLO/yolov5_master/runs/train/yolov5s_lineextract2/weights/best.pt'
+    # d_opt.weights = '/home/linxu/PycharmProjects/TrainNetHub/YOLO/yolov5_master/runs/train/yolov5s_towerlean/weights/best.pt'
+    d_opt.weights = '/home/linxu/PycharmProjects/TrainNetHub/YOLO/yolov5_master/runs/train/yolov5s_windbias/weights/best.pt'
+
     # 设置是否需要预览
     d_opt.view_img = False
     # 置信度设置
@@ -97,7 +100,6 @@ def detect_(object_name, models_name='yolov5s'):
     # 边界框线条粗细
     d_opt.line_thickness = 4
 
-    cv2.waitKey()
     """开始预测推理"""
     detect_main(d_opt)
 
@@ -109,14 +111,12 @@ if __name__ == '__main__':
         'smoke','insulator',
         'helmet','firesmoke',
         'plane_all','tower_only',
-        'tower_head'
+        'tower_head','towerlean'
     ]
 
-    # object_name = object_list[7]
-    object_name = 'tower_head'
-    # object_name = 'lineextract'
     # object_name = 'tower_body'
-    # object_name = 'foreignbody'
+    # object_name = 'towerlean'
+    object_name = 'windbias'
     # 模型选择
     models_list = [
         'yolov5n', 'yolov5n6',
@@ -125,15 +125,9 @@ if __name__ == '__main__':
         'yolov5l', 'yolov5m6',
         'yolov5x', 'yolov5x6',
     ]
-
-    # models_name = models_list[2]
-    continue_flag = False
-    # models_name = '/media/hxzh02/SB@home/hxzh/MyGithub/TrainNetHub/YOLO/yolov5_master/runs/train/yolov5s_tower_head4/weights/last.pt'
-    models_name = 'yolov5n'
-
-
+    models_name = models_list[2]
     # 模型训练
-    train_(object_name=object_name,models_name=models_name,continue_flag=continue_flag)
+    train_(object_name=object_name,models_name=models_name)
 
     # 模型预测
     # detect_(object_name)
